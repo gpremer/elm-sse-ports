f7cdb46d37e76d7badd6a41d574c2de6ebae928d
Geert Premereur <Geert.Premereur@gmail.com>
2016-11-06 09:36:52 +0100

    Moved Elm port setup to separate file

--- src/js/elm-ports.js
+++ src/js/elm-ports.js
@@ -1,1 +1,16 @@
+/*jslint browser: true*/
+/*global EventSource*/
+/*exported initElmPorts*/
+function initElmPorts(app) {
+    var sources = {};
 
+    app.ports.listenForTypedEvents.subscribe(function (addressAndEventType) {
+        var address = addressAndEventType[0];
+        var eventType = addressAndEventType[1];
+        console.log("Subscribing for events on " + address + " of type " + eventType);
+        sources[eventType] = new EventSource(address);
+        sources[eventType].addEventListener(eventType, function (event) {
+            app.ports.events.send({data: event.data || '', eventType: eventType, id: event.id || ''});
+        });
+    });
+}
