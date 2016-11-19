b9ddb231ece2281e71da0068dacc03839f5b2da0
Geert Premereur <Geert.Premereur@gmail.com>
2016-11-19 21:35:25 +0100

    Migrated to 0.18. Formatted in the process.

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,64 +1,83 @@
 port module Main exposing (..)
 
 import Html exposing (..)
-import Html.App as App
 import Html.Events exposing (..)
 import String
 import Json.Decode as Decode exposing (..)
 import Json.Decode.Extra as Decode exposing ((|:))
 import Maybe.Extra
-import Result.Extra as Result exposing (mapBoth)
-
+import Result.Extra as Result
 import SSE exposing (..)
 
-main =
-    App.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions}
 
-sseEndpoint = "http://localhost:8080/events"
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+sseEndpoint =
+    "http://localhost:8080/events"
+
+
 
 -- model
 
+
 type alias Point2d =
-    { x: Float
-    , y: Float
+    { x : Float
+    , y : Float
     }
+
 
 type alias Point3d =
-    { x: Float
-    , y: Float
-    , z: Float
+    { x : Float
+    , y : Float
+    , z : Float
     }
+
 
 type alias Model =
-    { point2d: Maybe String
-    , point3d: Maybe String
-    , generic: Maybe String
-    , sse: SseAccess Msg
+    { point2d : Maybe String
+    , point3d : Maybe String
+    , generic : Maybe String
+    , sse : SseAccess Msg
     }
 
-init: (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
     let
-        sseAccess = SSE.create sseEndpoint Noop
+        sseAccess =
+            SSE.create sseEndpoint Noop
     in
-        ( Model Nothing Nothing Nothing sseAccess , Cmd.none )
+        ( Model Nothing Nothing Nothing sseAccess, Cmd.none )
 
-points2dEventType: EventType
-points2dEventType = "2dpoint"
 
-points3dEventType: EventType
-points3dEventType = "3dpoint"
+points2dEventType : EventType
+points2dEventType =
+    "2dpoint"
 
-genericEvents: EventType
-genericEvents = "message"
+
+points3dEventType : EventType
+points3dEventType =
+    "3dpoint"
+
+
+genericEvents : EventType
+genericEvents =
+    "message"
+
+
 
 -- Update
 
-type Msg = New2dPoint (Result String Point2d)
+
+type Msg
+    = New2dPoint (Result String Point2d)
     | New3dPoint (Result String Point3d)
     | GenericEvent String
     | ListenFor2dPoints
@@ -69,14 +88,18 @@ type Msg = New2dPoint (Result String Point2d)
     | NoMoreGenericEvents
     | Noop
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        New2dPoint result -> ({ model | point2d = result |> Result.mapBoth identity showPoint2d |> Just }, Cmd.none)
+        New2dPoint result ->
+            ( { model | point2d = result |> Result.unpack identity showPoint2d |> Just }, Cmd.none )
 
-        New3dPoint result -> ({ model | point3d = result |> Result.mapBoth identity showPoint3d |> Just }, Cmd.none)
+        New3dPoint result ->
+            ( { model | point3d = result |> Result.unpack identity showPoint3d |> Just }, Cmd.none )
 
-        GenericEvent text -> ({ model | generic = Just text }, Cmd.none)
+        GenericEvent text ->
+            ( { model | generic = Just text }, Cmd.none )
 
         ListenFor2dPoints ->
             setSseAndDo model (withListener points2dEventType points2dEventDecoder)
@@ -86,7 +109,8 @@ update msg model =
 
         ListenForGenericEvents ->
             let
-                untypedDecoder =  \ev -> GenericEvent ev.data
+                untypedDecoder =
+                    \ev -> GenericEvent ev.data
             in
                 setSseAndDo model (withUntypedListener untypedDecoder)
 
@@ -99,95 +123,120 @@ update msg model =
         NoMoreGenericEvents ->
             setSseAndDo model withoutUntypedListener
 
-        Noop -> (model, Cmd.none) -- Silently drop events we don't know about. Can't happen anyway.
+        Noop ->
+            ( model, Cmd.none )
 
 
-setSseAndDo: Model -> (SseAccess Msg -> (SseAccess Msg, Cmd Msg)) -> (Model, Cmd Msg)
+
+{- Silently drop events we don't know about. Can't happen anyway. -}
+
+
+setSseAndDo : Model -> (SseAccess Msg -> ( SseAccess Msg, Cmd Msg )) -> ( Model, Cmd Msg )
 setSseAndDo model f =
     let
-        (sse, cmd) = f model.sse
+        ( sse, cmd ) =
+            f model.sse
     in
-        ( {model | sse = sse }
+        ( { model | sse = sse }
         , cmd
         )
 
-points2dEventDecoder: SsEvent -> Msg
+
+points2dEventDecoder : SsEvent -> Msg
 points2dEventDecoder event =
     New2dPoint (decodeString point2dDecoder event.data)
 
-points3dEventDecoder: SsEvent -> Msg
+
+points3dEventDecoder : SsEvent -> Msg
 points3dEventDecoder event =
     New3dPoint (decodeString point3dDecoder event.data)
 
-point2dDecoder: Decoder Point2d
+
+point2dDecoder : Decoder Point2d
 point2dDecoder =
     Decode.succeed Point2d
-        |: ("x" := Decode.float)
-        |: ("y" := Decode.float)
+        |: (field "x" Decode.float)
+        |: (field "y" Decode.float)
 
-point3dDecoder: Decoder Point3d
+
+point3dDecoder : Decoder Point3d
 point3dDecoder =
     Decode.succeed Point3d
-        |: ("x" := Decode.float)
-        |: ("y" := Decode.float)
-        |: ("z" := Decode.float)
+        |: (field "x" Decode.float)
+        |: (field "y" Decode.float)
+        |: (field "z" Decode.float)
 
-showPoint2d: Point2d -> String
+
+showPoint2d : Point2d -> String
 showPoint2d point =
     "(" ++ toString point.x ++ ", " ++ toString point.y ++ ")"
 
-showPoint3d: Point3d -> String
+
+showPoint3d : Point3d -> String
 showPoint3d point =
     "(" ++ toString point.x ++ ", " ++ toString point.y ++ ", " ++ toString point.z ++ ")"
 
+
+
 -- View
 
-view: Model -> Html Msg
+
+view : Model -> Html Msg
 view model =
     div []
-        [ p [] [text <| Maybe.withDefault "No 2d point yet" (Maybe.map (String.append "Last 2d point: ") model.point2d) ]
-        , p [] [text <| Maybe.withDefault "No 3d point yet" (Maybe.map (String.append "Last 3d point: ") model.point3d) ]
-        , p [] [text <| Maybe.withDefault "No generic event yet" (Maybe.map (String.append "Last generic event: ") model.generic) ]
+        [ p [] [ text <| Maybe.withDefault "No 2d point yet" (Maybe.map (String.append "Last 2d point: ") model.point2d) ]
+        , p [] [ text <| Maybe.withDefault "No 3d point yet" (Maybe.map (String.append "Last 3d point: ") model.point3d) ]
+        , p [] [ text <| Maybe.withDefault "No generic event yet" (Maybe.map (String.append "Last generic event: ") model.generic) ]
         , sseButtons model
         ]
+
 
 sseButtons : Model -> Html Msg
 sseButtons model =
     div [] <| List.map (\f -> f model) [ textButton, points2dButton, points3dButton ]
 
-textButton: Model -> Html Msg
-textButton model =
-    eventListeningToggleButton (listeningForGeneric  model ) "Listen for text events" "Stop listening for text events" ListenForGenericEvents NoMoreGenericEvents
 
-points2dButton: Model -> Html Msg
+textButton : Model -> Html Msg
+textButton model =
+    eventListeningToggleButton (listeningForGeneric model) "Listen for text events" "Stop listening for text events" ListenForGenericEvents NoMoreGenericEvents
+
+
+points2dButton : Model -> Html Msg
 points2dButton model =
     eventListeningToggleButton (listeningFor points2dEventType model) "Listen for 2d points" "Stop listening for 2d points" ListenFor2dPoints NoMore2dPoints
 
-points3dButton: Model -> Html Msg
+
+points3dButton : Model -> Html Msg
 points3dButton model =
     eventListeningToggleButton (listeningFor points3dEventType model) "Listen for 3d points" "Stop listening for 3d points" ListenFor3dPoints NoMore3dPoints
 
-listeningFor: EventType -> Model -> Bool
+
+listeningFor : EventType -> Model -> Bool
 listeningFor eventType model =
     SSE.hasListenerFor eventType model.sse
 
-listeningForGeneric: Model -> Bool
+
+listeningForGeneric : Model -> Bool
 listeningForGeneric model =
     Maybe.Extra.isJust model.sse.untypedDecoder
 
-eventListeningToggleButton: Bool -> String -> String -> Msg -> Msg -> Html Msg
+
+eventListeningToggleButton : Bool -> String -> String -> Msg -> Msg -> Html Msg
 eventListeningToggleButton on offText onText offMsg onMsg =
     let
-        (txt, msg) =
+        ( txt, msg ) =
             if on then
-                (onText, onMsg)
+                ( onText, onMsg )
             else
-                (offText, offMsg)
+                ( offText, offMsg )
     in
-        button [onClick msg] [text txt]
+        button [ onClick msg ] [ text txt ]
+
+
 
 -- Subscriptions
 
-subscriptions: Model -> Sub Msg
+
+subscriptions : Model -> Sub Msg
 subscriptions model =
     serverSideEvents model.sse
--- src/elm/SSE.elm
+++ src/elm/SSE.elm
@@ -1,20 +1,23 @@
-port module SSE exposing ( SsEvent
-                         , EventType
-                         , Endpoint
-                         , SseAccess
-                         , SseEventDecoder
-                         , hasListenerFor
-                         , create
-                         , withoutAnyListener
-                         , withListener
-                         , withUntypedListener
-                         , withoutListener
-                         , withoutUntypedListener
-                         , serverSideEvents
-                         )
+port module SSE
+    exposing
+        ( SsEvent
+        , EventType
+        , Endpoint
+        , SseAccess
+        , SseEventDecoder
+        , hasListenerFor
+        , create
+        , withoutAnyListener
+        , withListener
+        , withUntypedListener
+        , withoutListener
+        , withoutUntypedListener
+        , serverSideEvents
+        )
 
 import Dict exposing (..)
 import Maybe.Extra
+
 
 type alias SsEvent =
     { data : String
@@ -22,98 +25,122 @@ type alias SsEvent =
     , id : Maybe String
     }
 
+
 type alias UntypedSsEvent =
     { data : String
     , id : Maybe String
     }
 
+
+
 {-
-| A function that takes an SseEvent, of a specific type, and converts it to a domain-specific class.
+   | A function that takes an SseEvent, of a specific type, and converts it to a domain-specific class.
 -}
-type alias SseEventDecoder msg = SsEvent -> msg
 
-type alias UntypedSseEventDecoder msg = UntypedSsEvent -> msg
 
-type alias Endpoint = String
+type alias SseEventDecoder msg =
+    SsEvent -> msg
 
-type alias EventType = String
+
+type alias UntypedSseEventDecoder msg =
+    UntypedSsEvent -> msg
+
+
+type alias Endpoint =
+    String
+
+
+type alias EventType =
+    String
+
 
 type alias SseAccess msg =
-    { endpoint: Endpoint
-    , noopMessage: msg
+    { endpoint : Endpoint
+    , noopMessage : msg
     , decoders : Dict String (SseEventDecoder msg)
     , untypedDecoder : Maybe (UntypedSseEventDecoder msg)
     }
 
-hasListenerFor: EventType -> SseAccess msg -> Bool
+
+hasListenerFor : EventType -> SseAccess msg -> Bool
 hasListenerFor eventType sseAccess =
     Dict.member eventType sseAccess.decoders
+
 
 {-| Create a new SseAccess instance. This is instance is not yet listening for SSE events. That only happens when the
 first listener is attached.
 -}
-create: Endpoint -> msg -> SseAccess msg
+create : Endpoint -> msg -> SseAccess msg
 create endpoint noop =
     SseAccess endpoint noop Dict.empty Nothing
+
 
 {-| Stopping a subscription is not enough. Although that does free Elm from having to handle SSE event, we also need
  to release the resource for the browser and the server or the SSE socket remains open. Calling this method also
  clears all event handlers. If this sse instance is used again, listeners need to be added again.
 -}
-withoutAnyListener: SseAccess msg -> (SseAccess msg, Cmd msg)
+withoutAnyListener : SseAccess msg -> ( SseAccess msg, Cmd msg )
 withoutAnyListener sseAccess =
     ( { sseAccess | decoders = Dict.empty, untypedDecoder = Nothing }
-    , if hasListeners sseAccess then deleteEventSourceJS sseAccess.endpoint else Cmd.none
+    , if hasListeners sseAccess then
+        deleteEventSourceJS sseAccess.endpoint
+      else
+        Cmd.none
     )
+
 
 {-| Adds a listener for SSE events that don't have a type. This is very much like calling withListener with an event
 type of "message". In fact in the versions of Firefox and Chromium I tested against, it is completely the same at the
 Javascript level, but I don't see this behaviour mentioned in the spec. On the Elm side, this function has the benefit
  of calling eventually calling a decoder that doesn't need to handle the event type.
 -}
-withUntypedListener: UntypedSseEventDecoder msg -> SseAccess msg -> (SseAccess msg, Cmd msg)
+withUntypedListener : UntypedSseEventDecoder msg -> SseAccess msg -> ( SseAccess msg, Cmd msg )
 withUntypedListener eventDecoder sseAccess =
     let
         cmd =
             if hasListeners sseAccess then
-                addListenerJS (sseAccess.endpoint, Nothing)
-             else
-                createEventSourceAndAddListenerJS (sseAccess.endpoint, Nothing)
+                addListenerJS ( sseAccess.endpoint, Nothing )
+            else
+                createEventSourceAndAddListenerJS ( sseAccess.endpoint, Nothing )
     in
-    ( { sseAccess | untypedDecoder = Just eventDecoder }
-    , cmd
-    )
+        ( { sseAccess | untypedDecoder = Just eventDecoder }
+        , cmd
+        )
+
 
 {-| Adds a listener for a specific event type. Whenever such an event is received, it is passed as an SsEvent to the
 provided decoder function and the result of decoding is then pumped out of the subscription. If there is already a
 listener for the event type, it is replaced by the new one.
 -}
-withListener: EventType -> SseEventDecoder msg -> SseAccess msg -> (SseAccess msg, Cmd msg)
+withListener : EventType -> SseEventDecoder msg -> SseAccess msg -> ( SseAccess msg, Cmd msg )
 withListener eventType eventDecoder sseAccess =
     let
         cmd =
             if hasListeners sseAccess then
                 if Dict.member eventType sseAccess.decoders then
-                    Cmd.none -- All JS listener are the same, no need to set again
+                    Cmd.none
+                    -- All JS listener are the same, no need to set again
                 else
-                    addListenerJS (sseAccess.endpoint, Just eventType)
+                    addListenerJS ( sseAccess.endpoint, Just eventType )
             else
-                createEventSourceAndAddListenerJS (sseAccess.endpoint, Just eventType)
-
+                createEventSourceAndAddListenerJS ( sseAccess.endpoint, Just eventType )
     in
         ( { sseAccess | decoders = Dict.insert eventType eventDecoder sseAccess.decoders }
         , cmd
         )
 
+
 {-| Stops listening for untyped events. If no more listeners are active, also release the underlying SSE resource.
 -}
-withoutUntypedListener: SseAccess msg -> (SseAccess msg, Cmd msg)
+withoutUntypedListener : SseAccess msg -> ( SseAccess msg, Cmd msg )
 withoutUntypedListener sseAccess =
     let
-        sseAccessWithoutUntypedListener = { sseAccess | untypedDecoder = Nothing }
+        sseAccessWithoutUntypedListener =
+            { sseAccess | untypedDecoder = Nothing }
+
         cmd =
             if hasListeners sseAccessWithoutUntypedListener then
-                removeListenerJS (sseAccess.endpoint, Nothing)
+                removeListenerJS ( sseAccess.endpoint, Nothing )
             else
                 deleteEventSourceJS sseAccess.endpoint
     in
@@ -121,16 +148,19 @@ withoutUntypedListener sseAccess =
         , cmd
         )
 
+
 {-| Stops listening for events of the given type. If no more listeners are active, also release the underlying SSE
 resource.
 -}
-withoutListener: String -> SseAccess msg -> (SseAccess msg, Cmd msg)
+withoutListener : String -> SseAccess msg -> ( SseAccess msg, Cmd msg )
 withoutListener eventType sseAccess =
     let
-        sseAccessWithoutListener = { sseAccess | decoders = Dict.remove eventType sseAccess.decoders }
+        sseAccessWithoutListener =
+            { sseAccess | decoders = Dict.remove eventType sseAccess.decoders }
+
         cmd =
             if hasListeners sseAccessWithoutListener then
-                removeListenerJS (sseAccess.endpoint, Just eventType)
+                removeListenerJS ( sseAccess.endpoint, Just eventType )
             else
                 deleteEventSourceJS sseAccess.endpoint
     in
@@ -138,38 +168,56 @@ withoutListener eventType sseAccess =
         , cmd
         )
 
-hasListeners: SseAccess msg -> Bool
+
+hasListeners : SseAccess msg -> Bool
 hasListeners sseAccess =
-    (Maybe.Extra.isJust sseAccess.untypedDecoder) || not ( Dict.isEmpty sseAccess.decoders )
+    (Maybe.Extra.isJust sseAccess.untypedDecoder) || not (Dict.isEmpty sseAccess.decoders)
 
-port addListenerJS: (Endpoint, Maybe EventType) -> Cmd msg
 
-port removeListenerJS: (Endpoint, Maybe EventType) -> Cmd msg
+port addListenerJS : ( Endpoint, Maybe EventType ) -> Cmd msg
 
-port createEventSourceJS: Endpoint -> Cmd msg
 
-port deleteEventSourceJS: Endpoint -> Cmd msg
+port removeListenerJS : ( Endpoint, Maybe EventType ) -> Cmd msg
+
+
+port createEventSourceJS : Endpoint -> Cmd msg
+
+
+port deleteEventSourceJS : Endpoint -> Cmd msg
+
+
 
 -- Needed because Cmd.batch is not ordered
-port createEventSourceAndAddListenerJS: (Endpoint, Maybe EventType) -> Cmd msg
 
-serverSideEvents: SseAccess msg -> Sub msg
+
+port createEventSourceAndAddListenerJS : ( Endpoint, Maybe EventType ) -> Cmd msg
+
+
+serverSideEvents : SseAccess msg -> Sub msg
 serverSideEvents sseAccess =
     Sub.batch
         [ ssEventsJS <| decodersToEventMapper sseAccess
         , Maybe.withDefault Sub.none (Maybe.map ssUntypedEventsJS sseAccess.untypedDecoder)
         ]
 
-decodersToEventMapper: SseAccess msg -> SsEvent -> msg
+
+decodersToEventMapper : SseAccess msg -> SsEvent -> msg
 decodersToEventMapper sseAccess event =
-        let
-            maybeMsg = maybeMap (Dict.get event.eventType sseAccess.decoders) event -- by design we'll always find a decoder
-        in
-            Maybe.withDefault (sseAccess.noopMessage) maybeMsg
+    let
+        maybeMsg =
+            maybeMap (Dict.get event.eventType sseAccess.decoders) event
 
-maybeMap: Maybe (a -> b) -> a -> Maybe b
-maybeMap maybeF a = Maybe.map (\f -> f a) maybeF
+        -- by design we'll always find a decoder
+    in
+        Maybe.withDefault (sseAccess.noopMessage) maybeMsg
 
-port ssEventsJS: (SsEvent -> msg) -> Sub msg
 
-port ssUntypedEventsJS: (UntypedSsEvent -> msg) -> Sub msg
+maybeMap : Maybe (a -> b) -> a -> Maybe b
+maybeMap maybeF a =
+    Maybe.map (\f -> f a) maybeF
+
+
+port ssEventsJS : (SsEvent -> msg) -> Sub msg
+
+
+port ssUntypedEventsJS : (UntypedSsEvent -> msg) -> Sub msg
